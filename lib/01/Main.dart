d8f2bb0fa8cd298265dc688b656c2719ba6ee1b8
HG <hg.seo@xenoimpact.com>
2020-09-07 14:24:03 +0900

    Successfully moved codes and file-directory.

---
+++ lib/01/Main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(
+      MaterialApp(
+        title: 'Testing',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Testing an application"),
+          ),
+
+        ),
+      )
+  );
+}
