6cd8deb05a095bca1fd209915128363e09cc4869
김건우 <kw.kim@xenoimpact.com>
2020-09-14 17:36:15 +0900

    Testing

--- lib/Home.dart
+++ lib/Home.dart
@@ -69,7 +69,6 @@ class Body extends StatelessWidget{
                     child: new Text("Hi modal sheet"),
                   )
               ),
-              
               // padding: EdgeInsets.only(left: 12.0),
               // decoration: BoxDecoration(
               //   borderRadius: BorderRadius.circular(10.0),
