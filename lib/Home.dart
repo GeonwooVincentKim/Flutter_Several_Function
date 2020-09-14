6959edc4480ed8e6c834116982003ea74772a5cd
김건우 <kw.kim@xenoimpact.com>
2020-09-14 16:28:56 +0900

    Added underlines.

--- lib/Home.dart
+++ lib/Home.dart
@@ -21,24 +21,91 @@ class Home extends StatelessWidget {
 class Body extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Material(
-      color: Colors.lightBlueAccent,
-      child: Center(
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(left: 10.0, top: 20.0),
+        alignment: Alignment.topLeft,
+        color: Colors.black12,
         child: Column(
           children: <Widget>[
-            Text(
-              "Hello Flutter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 40.0)
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "Korean-Air",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 25.0,
+                        fontFamily: "Raleway",
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.black87
+                    ),
+                  ),
+                ),
+
+              ],
             ),
-            Text(
-              "Hi User",
-              textDirection: TextDirection.rtl,
-              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            const Divider(
+              color: Colors.black,
+              height: 40,
             ),
-          ]
-        )
-      ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "Korean-Air",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 25.0,
+                        fontFamily: "Raleway",
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.black87
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(
+              color: Colors.black,
+              height: 40,
+            ),
+          ],
+        ),
+      )
+
     );
+    // return Center(
+    //   child: Column(
+    //     children: <Widget>[
+    //       Expanded(
+    //         child: Container(
+    //           color: Colors.amber,
+    //           child: const Center(
+    //             child: Text('Above'),
+    //           ),
+    //         ),
+    //       ),
+    //       const Divider(
+    //         color: Colors.black,
+    //         height: 20,
+    //         thickness: 5,
+    //         indent: 20,
+    //         endIndent: 0,
+    //       ),
+    //       Expanded(
+    //         child: Container(
+    //           color: Colors.blue,
+    //           child: const Center(
+    //             child: Text('Below'),
+    //           ),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
