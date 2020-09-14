853156cb7235bebdd101f3c6d22d7fec420fd73e
김건우 <kw.kim@xenoimpact.com>
2020-09-14 17:17:15 +0900

    Before adding round-box

--- lib/Home.dart
+++ lib/Home.dart
@@ -51,6 +51,82 @@ class Body extends StatelessWidget{
               color: Colors.black,
               height: 40,
             ),
+
+            // Container(
+            //   padding: EdgeInsets.only(left: 1.0, top: 1.0),
+            //   color:Colors.white,
+            //   child: (
+            // Container(
+            //   padding: EdgeInsets.only(left: 12.0),
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(10.0),
+            //     color: Colors.green,
+            //   ),
+            //   height: 50,
+            //   child: Container(
+            //     decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.only(
+            //           topRight: Radius.circular(10.0),
+            //           bottomRight: Radius.circular(10.0)),
+            //       color: Colors.white,
+            //     ),
+            //     child: Text(
+            //       'This is a Container',
+            //       textScaleFactor: 2,
+            //       style: TextStyle(color: Colors.black),
+            //     ),
+            //   ),
+            // ),
+           // Center(
+              // child: ClipRect(
+              //   child: Align(
+              //     alignment: Alignment.topCenter,
+              //     heightFactor: 0.5,
+              //     child: Image.network("https://www.gstatic.com/webp/gallery/1.jpg"),
+              //   ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  color: Colors.transparent,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image(
+                              height: 100,
+                              width: 100,
+                              image: NetworkImage("https://www.gstatic.com/webp/gallery/1.jpg"),
+                            ),
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Text(
+                                "Title",
+                                textDirection: TextDirection.ltr,
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Text("Author name"),
+
+                              Divider(
+                                color: Colors.black,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+             // )
+            // ),
+
+            const Divider(
+              height: 40,
+            ),
             Row(
               children: <Widget>[
                 Expanded(
