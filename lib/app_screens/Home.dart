320ee45ed5f926f96aed0038bd4cc77cdd587de3
GeonwooKim <48337874+GeonwooVincentKim@users.noreply.github.com>
2020-09-20 19:33:54 +0900

    Makes Home.dart file shortly

--- lib/app_screens/Home.dart
+++ lib/app_screens/Home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/widgets/expanded/divider.dart';
 import 'package:flutter_app/widgets/expanded/expanded_widgets.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_app/widgets/expanded/body_details.dart';
 
 import '../shared/helpers/icomoon.dart';
 
@@ -153,28 +154,7 @@ class Body extends StatelessWidget{
 class Body_Details_Top extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // padding: EdgeInsets.only(left: 9.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        height: 70,
-        child: Row(
-          children: [
-            Expanded(
-                flex: 1,
-                // For Top-part Widgets, the Main
-                child: expanded_widgets_front_detail()
-            ),
-
-            Expanded(
-                flex: 3,
-                // For Below-part Widgets, the Main
-                child: expanded_widgets_behind_detail()
-            )
-          ],
-        ),
-    );
+    return listview_separated_up();
   }
 }
 
@@ -183,27 +163,6 @@ class Body_Details_Top extends StatelessWidget{
 class Body_Details_Below extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // padding: EdgeInsets.only(left: 9.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      height: 70,
-      child: Row(
-        children: [
-          Expanded(
-              flex: 1,
-              // For Top-part Widgets, the Main
-              child: expanded_widgets_left_detail()
-          ),
-
-          Expanded(
-              flex: 3,
-              // For Below-part Widgets, the Main
-              child: expanded_widgets_right_detail()
-          )
-        ],
-      ),
-    );
+    return listview_separated_down();
   }
 }
